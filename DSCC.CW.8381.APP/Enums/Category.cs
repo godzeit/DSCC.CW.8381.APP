974d1d0adbf299fa0e88ac8dc00bd286f2600a53
Godzeit <asrorov.davron@gmail.com>
2021-11-09 17:21:42 +0500

    Category enum added

---
+++ DSCC.CW.8381.APP/Enums/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSCC.CW._8381.APP.Enums
+{
+    public enum Category
+    {
+        Action,
+        Comedy,
+        Drama,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        Thriller,
+        Western,
+        Cartoon,
+    }
+}
